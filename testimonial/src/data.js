c8dded4dea0caa7f92339bfdc16c2b289dd7655e
santosh <sk1155610@gmail.com>
2024-09-21 18:20:17 +0530

    testimonial layout

---
+++ testimonial/src/data.js
@@ -0,0 +1,42 @@
+const reviews =[
+    
+        {
+          id: 1,
+          name: "Alex Johnson",
+          image: "https://randomuser.me/api/portraits/men/1.jpg",
+          job: "Frontend Developer",
+          review: "Alex is a creative problem solver. His attention to detail in UI design is impressive!"
+        },
+        {
+          id: 2,
+          name: "Maya Patel",
+          image: "https://randomuser.me/api/portraits/women/2.jpg",
+          job: "UX Designer",
+          review: "Maya's user-centric approach ensures delightful experiences. She's a great collaborator!"
+        },
+        {
+          id: 3,
+          name: "Liam Carter",
+          image: "https://randomuser.me/api/portraits/men/3.jpg",
+          job: "Data Analyst",
+          review: "Liam's analytical skills are unmatched. He dives deep into data and finds valuable insights."
+        },
+        {
+          id: 4,
+          name: "Emily Lee",
+          image: "https://randomuser.me/api/portraits/women/4.jpg",
+          job: "Marketing Specialist",
+          review: "Emily's creativity shines in every campaign. She knows how to engage our audience!"
+        },
+        {
+          id: 5,
+          name: "Rajesh Gupta",
+          image: "https://randomuser.me/api/portraits/men/5.jpg",
+          job: "Product Manager",
+          review: "Rajesh's strategic thinking drives product useful and success. and He's a true leader!             "
+        }
+      
+      
+];
+
+export default reviews;
