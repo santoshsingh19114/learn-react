62521ab37e74c1778d3a5cbfb4d29d5ac1f1bd5e
santosh <sk1155610@gmail.com>
2024-10-17 18:17:32 +0530

    use ref hook

---
+++ react_useref_project_a8a1377fab/react-useref-project/src/App.jsx
@@ -0,0 +1,84 @@
+
+import { useState,useEffect, useRef } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  let val = useRef(0);
+
+  let btnRef = useRef();
+
+  function handleIncrement() {
+    val.current = val.current + 1;
+    console.log("Value of val: ", val.current);
+    setCount(count +1);
+  }
+
+  //it runs on every render
+  useEffect(() => {
+    console.log("main ferse render hogya hu")
+  })
+  
+  function changeColor() {
+    btnRef.current.style.backgroundColor = "red";
+  }
+
+  const [time, setTime] = useState(0);
+
+  let timerRef = useRef(null);
+
+  function startTimer() {
+    timerRef.current = setInterval(() => {
+      setTime(time => time+1)
+    }, 1000);
+  }
+  function stopTimer() {
+    clearInterval(timerRef.current);
+    timerRef.current = null;
+  }
+  function resetTimer() {
+    stopTimer();
+    setTime(0);
+  }
+
+  return (
+    <div>
+
+
+       <h1>StopWatch: {time} seconds</h1>
+      <button onClick={startTimer}>
+        Start
+      </button>
+      <br/> <br/>
+
+      <button onClick={stopTimer}>
+        Stop
+      </button>
+      <br/> <br/>
+
+      <button onClick={resetTimer}>
+        Reset
+      </button> 
+
+
+       <button 
+      ref={btnRef}
+      onClick={handleIncrement}>
+        Increment
+      </button>
+      <br/>
+<br/>
+      <button onClick={changeColor}>
+        Change Color of 1st Button
+      </button>
+
+      <br/>
+
+      <div>
+        Count: {count}
+      </div> 
+    </div>
+  )
+}
+
+export default App
